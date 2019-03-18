e1c7351e40fda8641bae03048ec1ff25b1fd0834
Gremious <gremious@protonmail.com>
2019-03-18 20:36:04 +0000

    broken mirror

--- GremsReliquary/src/main/java/gremsReliquary/relics/BrokenMirror.java
+++ GremsReliquary/src/main/java/gremsReliquary/relics/BrokenMirror.java
@@ -1,10 +1,16 @@
 package gremsReliquary.relics;
 
-import basemod.abstracts.CustomRelic;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageRandomEnemyAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import gremsReliquary.GremsReliquary;
 
 public class BrokenMirror extends AbstractGremRelic {
@@ -12,39 +18,20 @@ public class BrokenMirror extends AbstractGremRelic {
     public static final String IMG = GremsReliquary.getModID() + "Resources/images/relics/placeholder_relic.png";
     public static final String OUTLINE = GremsReliquary.getModID() + "Resources/images/relics/outline/placeholder_relic.png";
     public static int amount = 2;
-    
-    private boolean usedThisTurn = false;
+    AbstractCreature p = AbstractDungeon.player;
     
     public BrokenMirror() {
         super(ID, ImageMaster.loadImage(IMG), new Texture(OUTLINE), RelicTier.UNCOMMON, LandingSound.CLINK);
     }
     
-    public void onEquip() {
-    
-    }
-    
-    public void atBattleStart() {
-    
-    }
-    
-    public void atTurnStart() {
-        
-    }
-    
-    public void onUseCard(AbstractCard targetCard, UseCardAction useCardAction) {
-    
-    }
-    
-    public void onPlayerEndTurn() {
-    
-    }
-    
-    public void onVictory() {
-    
-    }
-    
-    public void onUnequip() {
-    
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction useCardAction) {
+        if (card.type == AbstractCard.CardType.ATTACK
+                && AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
+            flash();
+            AbstractMonster rm = AbstractDungeon.getRandomMonster(null);
+            act(new DamageRandomEnemyAction(new DamageInfo(p, 2, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.BLUNT_LIGHT));
+        }
     }
     
     // Description
