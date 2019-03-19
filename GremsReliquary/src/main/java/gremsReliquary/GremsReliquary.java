fe826ae92590f0e6d744d8c26f8e50ffa558e160
Gremious <gremious@protonmail.com>
2019-03-19 12:39:18 +0000

    Revert "Revert "Merge branch 'feature/broken-mirror' into develop""
    This reverts commit 73495fd8d93256b4ea74b8a1469206213819957c.

--- GremsReliquary/src/main/java/gremsReliquary/GremsReliquary.java
+++ GremsReliquary/src/main/java/gremsReliquary/GremsReliquary.java
@@ -13,15 +13,16 @@ import com.evacipated.cardcrawl.modthespire.Loader;
 import com.evacipated.cardcrawl.modthespire.lib.SpireConfig;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.google.gson.Gson;
+<<<<<<< HEAD
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.FontHelper;
+=======
+import com.megacrit.cardcrawl.helpers.CardHelper;
+>>>>>>> feature/broken-mirror
 import com.megacrit.cardcrawl.localization.*;
-import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
-import gremsReliquary.effects.NeowTentacleEffect;
-import gremsReliquary.relics.NeowsTentacle;
-import gremsReliquary.relics.TimeIsMoney;
+import gremsReliquary.relics.*;
 import gremsReliquary.util.TextureLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,8 +36,8 @@ public class GremsReliquary implements
         EditRelicsSubscriber,
         EditStringsSubscriber,
         EditKeywordsSubscriber,
-        PostInitializeSubscriber,
-        RelicGetSubscriber {
+        PostInitializeSubscriber
+        /*,RelicGetSubscriber*/ {
     public static final Logger logger = LogManager.getLogger(GremsReliquary.class.getName());
     
     public static final boolean hasHalation;
@@ -55,6 +56,7 @@ public class GremsReliquary implements
     private static final String AUTHOR = "Gremious";
     private static final String DESCRIPTION = "A mod that adds any nice relic ideas I have.";
     
+<<<<<<< HEAD
     // Settings buttons
     public static Properties gremsReliquaryDefaultSettings = new Properties();
     public static final String PROP_ENABLE_NORMALS = "enableNormals";
@@ -62,6 +64,14 @@ public class GremsReliquary implements
     
     public static final String PROP_ENABLE_CURSED = "enableCursed";
     public static boolean enableCursed = false;
+=======
+    // =============== INPUT TEXTURE LOCATION =================
+    
+    // Potion Colors in RGB
+    public static final Color PLACEHOLDER_POTION_LIQUID = CardHelper.getColor(209.0f, 53.0f, 18.0f); // Orange-ish Red
+    public static final Color PLACEHOLDER_POTION_HYBRID = CardHelper.getColor(255.0f, 230.0f, 230.0f); // Near White
+    public static final Color PLACEHOLDER_POTION_SPOTS = CardHelper.getColor(100.0f, 25.0f, 10.0f); // Super Dark Red/Brown
+>>>>>>> feature/broken-mirror
     
     //Mod Badge - A small icon that appears in the mod settings menu next to your mod.
     public static final String BADGE_IMAGE = "gremsReliquaryResources/images/Badge.png";
@@ -105,6 +115,7 @@ public class GremsReliquary implements
         BaseMod.subscribe(this);
         setModID("gremsReliquary");
         
+<<<<<<< HEAD
         gremsReliquaryDefaultSettings.setProperty(PROP_ENABLE_NORMALS, "FALSE");
         try {
             SpireConfig config = new SpireConfig("gremsReliquary", "gremsReliquaryConfig", gremsReliquaryDefaultSettings);
@@ -150,6 +161,14 @@ public class GremsReliquary implements
     }// NO
     // ====== YOU CAN EDIT AGAIN ======
     
+=======
+        logger.info("Done subscribing");
+        
+        
+        logger.info("Done creating the color");
+    }
+    
+>>>>>>> feature/broken-mirror
     @SuppressWarnings("unused")
     public static void initialize() {
         logger.info("========================= Initializing Grem's Reliquary =========================");
@@ -170,6 +189,7 @@ public class GremsReliquary implements
         
         // Create the Mod Menu
         ModPanel settingsPanel = new ModPanel();
+<<<<<<< HEAD
         
         
         ModLabeledToggleButton enableNormalsButton = new ModLabeledToggleButton("Enable the normal relics",
@@ -202,6 +222,11 @@ public class GremsReliquary implements
         
         settingsPanel.addUIElement(enableNormalsButton);
         settingsPanel.addUIElement(enableCursedButton);
+=======
+        settingsPanel.addUIElement(new ModLabel("Grem's Reliquary doesn't have any settings!", 400.0f, 700.0f, settingsPanel, (me) -> {
+        }));
+        BaseMod.registerModBadge(badgeTexture, MODNAME, AUTHOR, DESCRIPTION, settingsPanel);
+>>>>>>> feature/broken-mirror
         
         logger.info("Done loading badge Image and mod options");
     }
@@ -216,11 +241,21 @@ public class GremsReliquary implements
         logger.info("Adding relics");
         
         // This adds a relic to the Shared pool. Every character can find this relic.
-        BaseMod.addRelic(new NeowsTentacle(), RelicType.SHARED);
+        //   BaseMod.addRelic(new NeowsTentacle(), RelicType.SHARED);
+        // UnlockTracker.markRelicAsSeen(NeowsTentacle.ID);
+        
         BaseMod.addRelic(new TimeIsMoney(), RelicType.SHARED);
+<<<<<<< HEAD
         
         UnlockTracker.markRelicAsSeen(NeowsTentacle.ID);
+=======
+        BaseMod.addRelic(new BrokenMirror(), RelicType.SHARED);
+        
+>>>>>>> feature/broken-mirror
         UnlockTracker.markRelicAsSeen(TimeIsMoney.ID);
+        UnlockTracker.markRelicAsSeen(BrokenMirror.ID);
+        
+        
         logger.info("Done adding relics!");
     }
     
@@ -242,6 +277,7 @@ public class GremsReliquary implements
         logger.info("Beginning to edit strings");
         
         // CardStrings
+<<<<<<< HEAD
         BaseMod.loadCustomStringsFile(CardStrings.class,
                 getModID() + "Resources/localization/eng/GremsReliquary-Card-Strings.json");
         
@@ -264,6 +300,18 @@ public class GremsReliquary implements
         // OrbStrings
         BaseMod.loadCustomStringsFile(OrbStrings.class,
                 getModID() + "Resources/localization/eng/GremsReliquary-Orb-Strings.json");
+=======
+        BaseMod.loadCustomStringsFile(CardStrings.class, getModID() + "Resources/localization/eng/GremsReliquary-Card-Strings.json");
+        
+        // PowerStrings
+        BaseMod.loadCustomStringsFile(PowerStrings.class, getModID() + "Resources/localization/eng/GremsReliquary-Power-Strings.json");
+        
+        // RelicStrings
+        BaseMod.loadCustomStringsFile(RelicStrings.class, getModID() + "Resources/localization/eng/GremsReliquary-Relic-Strings.json");
+        
+        // OrbStrings
+        BaseMod.loadCustomStringsFile(OrbStrings.class, getModID() + "Resources/localization/eng/GremsReliquary-Orb-Strings.json");
+>>>>>>> feature/broken-mirror
         
         logger.info("Done edittting strings");
     }
@@ -295,6 +343,7 @@ public class GremsReliquary implements
     
     // ================ /LOAD THE KEYWORDS/ ===================
     
+<<<<<<< HEAD
     // this adds "ModName:" before the ID of any card/relic/power etc.
     // in order to avoid conflicts if any other mod uses the same ID.
     public static String makeID(String idText) {
@@ -302,6 +351,9 @@ public class GremsReliquary implements
     }
     
     @Override
+=======
+  /*  @Override
+>>>>>>> feature/broken-mirror
     public void receiveRelicGet(AbstractRelic obtainedRelic) {
         System.out.println("HEY A RELIC WAS OBTAINED!: " + obtainedRelic.relicId);
         if (obtainedRelic.relicId.equals(NeowsTentacle.ID)) {
@@ -309,5 +361,41 @@ public class GremsReliquary implements
             // obtainedRelic.onTrigger();
             
         }
+}*/
+    
+    // ====== NO EDIT AREA ======
+    // DON'T TOUCH THIS STUFF. IT IS HERE FOR STANDARDIZATION BETWEEN MODS AND TO ENSURE GOOD CODE PRACTICES.
+    // IF YOU MODIFY THIS I WILL HUNT YOU DOWN AND DOWNVOTE YOUR MOD ON WORKSHOP
+    
+    public static void setModID(String ID) { // DON'T EDIT
+        if (ID.equals("theDefault")) { // DO *NOT* CHANGE THIS ESPECIALLY, TO EDIT YOUR MOD ID, SCROLL UP JUST A LITTLE, IT'S JUST ABOVE
+            throw new RuntimeException("Go to your constructor in your class with SpireInitializer and change your mod ID from \"theDefault\""); // THIS ALSO DON'T EDIT
+        } else if (ID.equals("theDefaultDev")) { // NO
+            modID = "theDefault"; // DON'T
+        } else { // NO EDIT AREA
+            modID = ID; // DON'T WRITE OR CHANGE THINGS HERE NOT EVEN A LITTLE
+        } // NO
+    } // NO
+    
+    public static String getModID() { // NO
+        return modID; // DOUBLE NO
+    } // NU-UH
+    
+    private static void pathCheck() { // ALSO NO
+        String packageName = GremsReliquary.class.getPackage().getName(); // STILL NOT EDIT ZONE
+        FileHandle resourcePathExists = Gdx.files.internal(getModID() + "Resources"); // PLEASE DON'T EDIT THINGS
+        if (!modID.equals("theDefaultDev")) { // LEAVE THIS EDIT-LESS
+            if (!packageName.equals(getModID())) { // NOT HERE ETHER
+                throw new RuntimeException("Rename your gremsReliquary folder to match your mod ID! " + getModID()); // THIS IS A NO-NO
+            } // WHY WOULD U EDIT THIS
+            if (!resourcePathExists.exists()) { // DON'T CHANGE THIS
+                throw new RuntimeException("Rename your gremsReliquaryResources folder to match your mod ID! " + getModID() + "Resources"); // NOT THIS
+            }// NO
+        }// NO
+    }// NO
+    // ====== YOU CAN EDIT AGAIN ======
+    
+    public static String makeID(String idText) {
+        return getModID() + ":" + idText;
     }
 }
